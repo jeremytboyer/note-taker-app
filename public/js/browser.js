d610b7554dccf9ea605f13ca9f7637cc199ec631
Jeremy Boyer <jeremytboyer@gmail.com>
2023-07-02 11:47:42 -0400

    added form control

--- public/js/browser.js
+++ public/js/browser.js
@@ -1,6 +1,8 @@
 const titleVal = document.querySelector("#title");
 const noteVal = document.querySelector("#note-text");
 const saveBtn = document.querySelector("#save");
+const plusBtn = document.querySelector('#plus')
+const form = document.querySelector('#note-form')
 
 titleVal.addEventListener('input', function() {
   if (titleVal.value.length > 0) {
@@ -9,3 +11,10 @@ titleVal.addEventListener('input', function() {
     saveBtn.classList.add('hidden');
   }
 });
+
+saveBtn.addEventListener('click', () => {
+  form.submit()
+})
+
+
+
