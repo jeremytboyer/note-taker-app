f5430181db20d8d427a146f17aec00532741ec94
Jeremy Boyer <jeremytboyer@gmail.com>
2023-07-02 10:42:26 -0400

    added browser.js and Note.js

---
+++ models/Note.js
@@ -0,0 +1,30 @@
+const { v4 } = require('uuid')
+const fs = require('fs')
+
+
+const DB_PATH = path.join(__dirname, "../db/notes.json");
+
+function getNotes() {
+  const notes = JSON.parse(fs.readFileSync(DB_PATH, "utf8"));
+  return notes;
+}
+
+class Note {
+  constructor(title, text){
+    this.id = v4();
+    this.title = title;
+    this.text = text;
+  }
+  save() {
+    const notes = getNotes();
+    notes.push(this);
+
+    fs.writeFile(DB_PATH, JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw err;
+  })
+
+
+}}
+
+
+module.exports = {Note: Note, getNotes: getNotes};
--- public/js/browser.js
+++ public/js/browser.js
@@ -1,1 +1,11 @@
-// write 
+const titleVal = document.querySelector("#title");
+const noteVal = document.querySelector("#note-text");
+const saveBtn = document.querySelector("#save");
+
+titleVal.addEventListener('input', function() {
+  if (titleVal.value.length > 0) {
+    saveBtn.classList.remove('hidden');
+  } else {
+    saveBtn.classList.add('hidden');
+  }
+});
